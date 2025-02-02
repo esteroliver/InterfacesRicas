312b264435e0ecd4baca28e963af232dbcf7e9f6
Ester Oliveira <oliver.ester.2004@gmail.com>
2025-02-02 17:21:01 -0300

    style: input para adicionar prioridade da tarefa

--- projeto-angular/src/app/components/inserir-tarefa/inserir-tarefa.component.ts
+++ projeto-angular/src/app/components/inserir-tarefa/inserir-tarefa.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { Tarefa } from '../tarefa';
 
 @Component({
@@ -9,7 +10,8 @@ import { Tarefa } from '../tarefa';
   imports: [
     InputTextModule,
     FloatLabelModule,
-    ButtonModule
+    ButtonModule,
+    InputNumberModule
   ],
   templateUrl: './inserir-tarefa.component.html',
 })
