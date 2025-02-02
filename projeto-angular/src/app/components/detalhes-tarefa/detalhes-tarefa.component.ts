f813be180c4ad21ada5e371009a946465ddfce25
Ester Oliveira <oliver.ester.2004@gmail.com>
2025-02-02 16:36:18 -0300

    feat: componentes tarefa e detalhes-tarefa

---
+++ projeto-angular/src/app/components/detalhes-tarefa/detalhes-tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-tarefa',
+  imports: [],
+  templateUrl: './detalhes-tarefa.component.html',
+  styleUrl: './detalhes-tarefa.component.css'
+})
+export class DetalhesTarefaComponent {
+
+}
